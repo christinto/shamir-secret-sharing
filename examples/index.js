491d45b6b325e4175185c3b941181e141493c580
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-05-06 17:08:12 +0530

    add examples

---
+++ examples/index.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var secrets = require('../secrets.js');
+
+var threshold = 5;
+var totalShares = 10;
+
+
+// generate a 512-bit key
+var key = secrets.random(512); // => key is a hex string
+console.log("Randomly Generated Key:",key,"\n");
+
+
+// split into 10 shares with a threshold of 5
+var shares = secrets.share(key,totalShares, threshold);
+console.log("Array of shares with threshold",threshold,"within a total of",totalShares,"shared pieces:  \n",shares,"\n");
+
+// combining 4 shares to check if it works
+
+testKey = shares.slice(0,4)
+var comb = secrets.combine( testKey );
+console.log("Keys used:",testKey.length,"\n",testKey,"\n Can create original secret:", comb === key,"\n");
+
+
+// combine 5 shares to verify if it works
+testKey = shares.slice(4,9)
+comb = secrets.combine( testKey );
+console.log("Keys used:",testKey.length,"\n",testKey,"\n Can create original secret:", comb === key,"\n");
